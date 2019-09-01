aa72079730f55480a345d3ba5386baf36c428ade
amyer <amyers1156228@gmail.com>
2019-09-01 14:33:07 -0400

    Once again

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,10 +7,7 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		for (i = 0; i < scan; i++) {
-			System.out.println()
-		}
+		
 		
 	}
 }
