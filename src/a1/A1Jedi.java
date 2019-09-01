24af2f7398da1977b7384eaf083ce168ea201d4d
amyer <amyers1156228@gmail.com>
2019-09-01 18:01:53 -0400

    Done!

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,45 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int storeItemsAmount = scan.nextInt();
+		String[] storeItems = new String[storeItemsAmount];
+		int[] storeItemsAmountBought = new int[storeItemsAmount];
+		int[] customersBought = new int[storeItemsAmount];
+		
+		for (int i = 0; i < storeItemsAmount; i++) {
+			storeItems[i] = scan.next();
+			scan.nextDouble();
+		}
+		
+		int customers = scan.nextInt();
+		
+		for (int i = 0; i < customers; i++) {
+			scan.next();
+			scan.next();
+			
+			int itemsBought = scan.nextInt();
+			
+			for (int j = 0; j < itemsBought; j++) {
+				int amountBought = scan.nextInt();
+				String itemBought = scan.next();
+				
+				for (int k = 0; k < storeItems.length; k++) {
+					if (storeItems[k].equals(itemBought)) {
+						customersBought[k] = 1 + customersBought[k];
+						storeItemsAmountBought[k] = amountBought + storeItemsAmountBought[k];
+					}
+				}
+				
+			}
+				
+		}
+		for (int i = 0; i < storeItems.length; i++) {
+			if (customersBought[i] == 0) {
+				System.out.println("No customers bought " + storeItems[i]);
+			} else {
+				System.out.println(customersBought[i] + " customers bought " + storeItemsAmountBought[i] + " " + storeItems[i]);
+			}
+		}
 		
 	}
 }
